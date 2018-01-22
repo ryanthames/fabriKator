61111682544768ce2aa94d9d82a566990a705593
Ryan Thames <ryanthames@gmail.com>
2018-01-21 21:26:54 -0600

    Throw UndefinedFactoryException if factory doesn't exist

--- src/main/kotlin/com/ocsoftware/Fabrikator.kt
+++ src/main/kotlin/com/ocsoftware/Fabrikator.kt
@@ -1,10 +1,22 @@
 package com.ocsoftware
 
 import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredFunctions
 
 open class Fabrikator {
-  // TODO
-  fun build(klass: KClass<*>, attrs: List<Pair<*, *>> = emptyList()) {
-    // todo
+  fun build(klass: KClass<*>, attrs: List<Pair<*, *>> = emptyList()): Any? {
+    val filteredFunctions = this::class.declaredFunctions.filter { it.name == "${klass.simpleName}Factory" }
+
+    if (filteredFunctions.isEmpty()) {
+      throw UndefinedFactoryException("""
+      No factory defined for ${klass.simpleName}. Please check for typos or define your factory:
+
+      def ${klass.simpleName}Factory(): ${klass.simpleName} {
+        ...
+      }
+      """.trimIndent())
+    }
+
+    return filteredFunctions[0].call()
   }
 }
